148c145fbfac632f5277e7760e847e41df48f0c6
EslamKhder <eslamkhder81@gmail.com>
2021-05-10 00:34:13 +0200

    HomeComponent,ProfileComponent,SocialComponent and Routes

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componants/header/header.component';
+import { HomeComponent } from './componants/home/home.component';
+import { ProfileComponent } from './componants/profile/profile.component';
+import { SocialComponent } from './componants/social/social.component';
+import {RouterModule, Routes} from '@angular/router';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: SocialComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: '**', redirectTo: '/home'}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ProfileComponent,
+    SocialComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
