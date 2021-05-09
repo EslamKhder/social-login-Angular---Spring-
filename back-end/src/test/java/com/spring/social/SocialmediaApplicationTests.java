622d2488c17b72776192af068f5e1e5cbca6bf8c
EslamKhder <eslamkhder81@gmail.com>
2021-05-09 22:35:20 +0200

    Create back-end (Spring)

---
+++ back-end/src/test/java/com/spring/social/SocialmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
