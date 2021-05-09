538392f04bbeea0f263816c3ae6d3c6989551923
EslamKhder <eslamkhder81@gmail.com>
2021-05-10 01:47:26 +0200

    Sign in and out users

--- front-end/src/app/componants/social/social.component.ts
+++ front-end/src/app/componants/social/social.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FacebookLoginProvider, GoogleLoginProvider, SocialAuthService} from 'angularx-social-login';
 
 @Component({
   selector: 'app-social',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SocialComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: SocialAuthService) { }
 
   ngOnInit(): void {
   }
 
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
 }
