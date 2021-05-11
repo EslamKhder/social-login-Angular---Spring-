30e69ba09bc13855d89029c147512c02668b263e
EslamKhder <eslamkhder81@gmail.com>
2021-05-12 00:59:46 +0200

    Angular Show data of User

--- front-end/src/app/componants/header/header.component.ts
+++ front-end/src/app/componants/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SocialAuthService} from 'angularx-social-login';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isLogin: boolean; // false
+  constructor(private authService: SocialAuthService) { }
 
   ngOnInit(): void {
+    this.authService.authState.subscribe(
+      data => {
+        this.isLogin = (data != null);
+      }
+    );
   }
 
 }
--- front-end/src/app/componants/profile/profile.component.ts
+++ front-end/src/app/componants/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SocialAuthService, SocialUser} from 'angularx-social-login';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: SocialUser;
+  isLogin: boolean; // false
+  constructor(private authService: SocialAuthService) { }
 
   ngOnInit(): void {
+    this.authService.authState.subscribe(
+      data => {
+        this.isLogin = (data != null);
+        this.user = data;
+      }
+    );
   }
 
 }
